48c4bcd7b9b2efe398989f09305e7e92c1337720
Vijay <vijayotaradi11@gmail.com>
2025-01-25 09:52:29 +0530

    App2.js

---
+++ Day 03 25-01-2025/demo/src/App2.js
@@ -0,0 +1,13 @@
+import React,{ Component } from "react";
+
+export default class App2 extends Component
+{
+    render()
+    {
+        return(
+            <>
+            <h1>App 2</h1>
+            </>
+        );
+    }
+}
